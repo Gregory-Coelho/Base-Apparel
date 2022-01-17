ba20a2dd3feda488f329c80fd9930fffce249ad6
Gregory <gregory.coelho21@gmail.com>
2022-01-17 23:17:25 +0100

    bug of left background fixed

--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -1,21 +1,20 @@
 import { useState } from "react";
 import button from "../assets/icon-arrow.svg";
 import styles from "../styles/Email.module.css";
+import error from "../assets/icon-error.svg";
 export function Email() {
   const [inputValue, setInputValue] = useState("");
 
   function handleInput(e: any) {
     setInputValue(e.target.value);
   }
+  const invalid = !inputValue.includes("@");
 
-  function invalid() {
-    if (!inputValue.includes("@")) {
-      /*("Please profide a valid email");*/
-    }
-  }
-  return (
+  return invalid ? (
     <div>
       <form className={styles.formEmail}>
+        <img src={error} className={styles.errorIcon} />
+
         <input
           className={styles.inputEmail}
           placeholder="Email Address"
@@ -27,6 +26,20 @@ export function Email() {
           <img src={button} />
         </button>
       </form>
+      <p className={styles.messageError}> Please profide a valid email</p>
     </div>
+  ) : (
+    <form className={styles.formEmail}>
+      <input
+        className={styles.inputEmail}
+        placeholder="Email Address"
+        value={inputValue}
+        type="email"
+        onChange={handleInput}
+      />
+      <button type="submit" className={styles.buttonEmail}>
+        <img src={button} />
+      </button>
+    </form>
   );
 }

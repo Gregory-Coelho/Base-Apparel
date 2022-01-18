63aff87732c1eb5062ca6726f0d39a56c6f7fb53
Gregory <gregory.coelho21@gmail.com>
2022-01-18 17:32:59 +0100

    project finish ;D

--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -3,43 +3,43 @@ import button from "../assets/icon-arrow.svg";
 import styles from "../styles/Email.module.css";
 import error from "../assets/icon-error.svg";
 export function Email() {
-  const [inputValue, setInputValue] = useState("");
-
-  function handleInput(e: any) {
-    setInputValue(e.target.value);
-  }
-  const invalid = !inputValue.includes("@");
-
-  return invalid ? (
+  const [hasError, sethasError] = useState(Boolean);
+  const logoError = <img src={error} className={styles.errorIcon} />;
+  const paraError = (
+    <p className={styles.messageError}> Please provide a valid email</p>
+  );
+  return (
     <div>
       <form className={styles.formEmail}>
-        <img src={error} className={styles.errorIcon} />
-
+        {hasError && logoError}
         <input
-          className={styles.inputEmail}
+          className={`${hasError ? styles.inputEmailError : ""} ${
+            styles.inputEmail
+          }`}
           placeholder="Email Address"
-          value={inputValue}
           type="email"
-          onChange={handleInput}
+          onChange={(e) => {
+            const newValue = e.target.value;
+
+            if (newValue.includes("@") || newValue === "") {
+              sethasError(false);
+            }
+          }}
+          onBlur={(e) => {
+            const newValue = e.target.value;
+
+            if (newValue && !newValue.includes("@")) {
+              {
+                sethasError(true);
+              }
+            }
+          }}
         />
         <button type="submit" className={styles.buttonEmail}>
           <img src={button} />
         </button>
       </form>
-      <p className={styles.messageError}> Please provide a valid email</p>
+      {hasError && paraError}
     </div>
-  ) : (
-    <form className={styles.formEmail}>
-      <input
-        className={styles.inputEmail}
-        placeholder="Email Address"
-        value={inputValue}
-        type="email"
-        onChange={handleInput}
-      />
-      <button type="submit" className={styles.buttonEmail}>
-        <img src={button} />
-      </button>
-    </form>
   );
 }
